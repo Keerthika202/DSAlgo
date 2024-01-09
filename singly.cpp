db9c4b37e403b482488d8ceedc16df30a62afefb
Keerthika202 <101097856+Keerthika202@users.noreply.github.com>
2024-01-09 19:56:15 +0530

    Update singly.cpp

--- singly.cpp
+++ singly.cpp
@@ -8,7 +8,7 @@ struct Node* head = NULL;
 void insertatStart(int new_data) {
    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
    new_node->data = new_data;
-   new_node->next = head;
+   new_node->next = head;// since its starting node
    head = new_node;
 }
 void insertEnd(Node** head, int data)
@@ -17,11 +17,10 @@ void insertEnd(Node** head, int data)
     Node* new_node = new Node();
 
     new_node->data = data;
-    new_node->next = NULL;
+    new_node->next = NULL;// since its ending node
     if(*head==NULL)
     {
-        *head = new_node;
-        cout << new_node->data << " inserted" << endl; 
+        *head = new_node;// the added element will be the head
         return; 
     }
  
